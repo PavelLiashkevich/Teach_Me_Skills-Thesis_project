b3b3a4bf9210f8cce33a7b18c79b8bcaa7f19026
Pavel Liashkevich <75558987+PavelLiashkevich@users.noreply.github.com>
2021-07-17 03:15:56 +0300

    Update AddedProduct.jsx

--- src/components/Basket/AddedProduct.jsx
+++ src/components/Basket/AddedProduct.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 export const AddedProduct = ({ image, title, price, category }) => {
-	const [count, setCount] = useState(0);
+	const [count, setCount] = useState(1);
 
 	const inc = () => {
 		setCount(count + 1);
